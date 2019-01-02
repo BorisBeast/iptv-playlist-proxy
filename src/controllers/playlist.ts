db4467b112767b7c1b21b02f85f0ff8f63bcc502
bjbeast@gmail.com <boris.jovanovic@limitforex.com>
2019-01-02 15:34:00 +0100

    make diff of previous and current playlist

--- src/controllers/playlist.ts
+++ src/controllers/playlist.ts
@@ -1,3 +1,4 @@
+import * as diff from 'diff';
 import { Request, Response } from 'express';
 import fs from 'fs';
 import request from 'request-promise-native';
@@ -10,54 +11,86 @@ class PlaylistController {
   }
 
   get(req: Request, res: Response) {
-    logger.info('requesting playlist from ' + process.env.PLAYLIST_URL);
+    logger.info('===== requesting playlist from %s', process.env.PLAYLIST_URL);
     request({
       uri: process.env.PLAYLIST_URL,
       simple: true,
       resolveWithFullResponse: true
     })
       .then(response => {
-        logger.info('request successful');
-        this.sendFile(response.body, res);
+        logger.info('playlist request successful');
+        this.diffAndSendFile(response.body, res);
       })
       .catch(error => {
-        // const response = error.response;
-        // if (response) {
-        //   res.status(response.statusCode).send(response.body);
-        // } else {
-        //   res.status(500).send(error.message);
-        // }
-        logger.error('request failed %o', error);
-        this.sendFile(undefined, res);
+        logger.error('playlist request failed %o', JSON.parse(JSON.stringify(error)));
+        this.diffAndSendFile(undefined, res);
+      })
+      .then(() => {
+        logger.info('===== done');
       });
   }
 
-  sendFile(fileContent: string, response: Response) {
+  diffAndSendFile(fileContent: string, response: Response) {
+    const cachedFilePath = process.env.OUTPUT_DIR + '/tv_channels.m3u';
+    let cachedFileContent: string;
+    let shouldCache = false;
+    try {
+      logger.info('reading cached file');
+      cachedFileContent = fs.readFileSync(cachedFilePath).toString();
+    } catch (err) {
+      logger.error('reading cached file failed %o', err);
+    }
+
+    if (!fileContent && !cachedFileContent) {
+      response.status(404).send('File not found');
+      return;
+    }
+
+    response
+      .header({
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': 'attachment; filename="tv_channels.m3u"'
+      })
+      .send(fileContent || cachedFileContent);
+
     if (fileContent) {
-      try {
-        fs.writeFileSync(process.env.OUTPUT_DIR + '/tv_channels.m3u', fileContent);
-      } catch (err) {
-        response.status(500).send(err.message);
-        return;
-      }
-    } else {
-      try {
-        fileContent = fs.readFileSync(process.env.OUTPUT_DIR + '/tv_channels.m3u').toString();
-      } catch (err) {
-        if (err.code === 'ENOENT') {
-          response.status(404).send('File not found');
+      if (cachedFileContent) {
+        logger.info('calculating diff');
+        const changes = diff.createPatch(
+          'tv_channels.m3u',
+          cachedFileContent,
+          fileContent,
+          'cached file',
+          'new file',
+          { context: 3 }
+        );
+        if (changes.indexOf('@@') !== -1) {
+          shouldCache = true;
+
+          try {
+            const diffPath =
+              process.env.OUTPUT_DIR + '/tv_channels_' + Date.now() + '.diff';
+            logger.info('writing diff file %s', diffPath);
+            fs.writeFileSync(diffPath, changes);
+          } catch (err) {
+            logger.error('writing diff file failed %o', err);
+          }
         } else {
-          response.status(500).send(err.message);
+          logger.info("playlist didn't change");
         }
-        return;
+      } else {
+        shouldCache = true;
       }
     }
-    response
-    .header({
-      'Content-Type': 'application/octet-stream',
-      'Content-Disposition': 'attachment; filename="tv_channels.m3u"'
-    })
-    .send(fileContent);
+
+    if (shouldCache) {
+      try {
+        logger.info('caching file');
+        fs.writeFileSync(cachedFilePath, fileContent);
+      } catch (err) {
+        logger.error('caching failed %o', err);
+      }
+    }
   }
 }
 
